9afa0d7d50ec2360096cbd024fbd23f277be266c
dejetem <52535603+dejetem@users.noreply.github.com>
2021-04-30 09:40:29 +0100

    change

--- blogapp/blogapp/settings.py
+++ blogapp/blogapp/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,4 +133,3 @@ LOGOUT_REDIRECT_URL = 'home'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Activate Django-Heroku.
-django_heroku.settings(locals())
