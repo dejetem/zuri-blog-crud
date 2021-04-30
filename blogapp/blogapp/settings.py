8d71c024bd80162a770e3235ec2a83bbf9c3fdf1
dejetem <52535603+dejetem@users.noreply.github.com>
2021-04-30 15:10:11 +0100

    done

--- blogapp/blogapp/settings.py
+++ blogapp/blogapp/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6q*0599fpjwl3jpx0!p)2in_ro3h9k7n8!sg#d^+m4giwmw823'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
